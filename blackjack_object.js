06be827957b0abdbee8e1f323241b6bc4900ee18
tugaincdev <tugaincdev@gmail.com>
2025-10-13 15:53:12 +0100

    Update blackjack_object.js

--- blackjack_object.js
+++ blackjack_object.js
@@ -1,1 +1,113 @@
 
+// Blackjack object
+
+/**
+ * Class that represents the Blackjack game.
+ */
+class Blackjack {
+    // Constant that defines the maximum points to avoid busting in Blackjack
+    static MAX_POINTS = 25;
+    // Constant that defines the point threshold at which the dealer must stand
+    static DEALER_MAX_TURN_POINTS = 21;
+
+    /**
+     * Creates an instance of Blackjack and initializes the deck.
+     */
+    constructor() {
+        this.dealerCards = []; // Array to hold the dealer's cards
+        this.playerCards = []; // Array to hold the player's cards
+        this.dealerTurn = false; // Flag to indicate if it's the dealer's turn to play
+
+        // State of the game with information about the outcome
+        this.state = {
+            gameEnded: false, // Indicates whether the game has ended
+            playerWon: false, // Indicates if the player has won
+            dealerWon: false, // Indicates if the dealer has won
+            playerBusted: false, // Indicates if the player has exceeded MAX_POINTS
+            dealerBusted: false // Indicates if the dealer has exceeded MAX_POINTS
+        };
+
+        // Initialize the deck of cards
+        this.deck = this.shuffle(this.newDeck()); // Create and shuffle a new deck
+    }
+
+    //TODO: Implement this method
+    /**
+     * Creates a new deck of cards.
+     * @returns {Card[]} - An array of cards.
+     */
+    newDeck() {
+
+    }
+
+    //TODO: Implement this method
+    /**
+     * Shuffles the deck of cards.
+     * @param {Card[]} deck - The deck of cards to be shuffled.
+     * @returns {Card[]} - The shuffled deck.
+     */
+    shuffle(deck) {
+
+    }
+
+    /**
+     * Returns the dealer's cards.
+     * @returns {Card[]} - An array containing the dealer's cards.
+     */
+    getDealerCards() {
+        return this.dealerCards.slice(); // Return a copy of the dealer's cards
+    }
+
+    /**
+     * Returns the player's cards.
+     * @returns {Card[]} - An array containing the player's cards.
+     */
+    getPlayerCards() {
+        return this.playerCards.slice(); // Return a copy of the player's cards
+    }
+
+    /**
+     * Sets whether it is the dealer's turn to play.
+     * @param {boolean} val - Value indicating if it's the dealer's turn.
+     */
+    setDealerTurn(val) {
+        this.dealerTurn = val; // Update the dealer's turn status
+    }
+
+    //TODO: Implement this method
+    /**
+     * Calculates the total value of the provided cards.
+     * @param {Card[]} cards - Array of cards to be evaluated.
+     * @returns {number} - The total value of the cards.
+     */
+    getCardsValue(cards) {
+
+    }
+
+    //TODO: Implement this method
+    /**
+     * Executes the dealer's move by adding a card to the dealer's array.
+     * @returns {Object} - The game state after the dealer's move.
+     */
+    dealerMove() {
+
+    }
+
+    //TODO: Implement this method
+    /**
+     * Executes the player's move by adding a card to the player's array.
+     * @returns {Object} - The game state after the player's move.
+     */
+    playerMove() {
+
+    }
+
+    //TODO: Implement this method
+    /**
+     * Checks the game state based on the dealer's and player's cards.
+     * @returns {Object} - The updated game state.
+     */
+    getGameState() {
+
+    }
+}
