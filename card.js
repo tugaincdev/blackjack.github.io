e9d9bc1422910b0f0b36e0d81950d019eff50c6f
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-20 16:10:48 +0100

    Update card.js
    printName corrected

--- card.js
+++ card.js
@@ -1,62 +1,58 @@
 class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value; //not game value; has 11,12,13
+  }
 
-
-
-    constructor(suit, value) {
-        this.suit = suit;
-        this.value = value; //not game value; has 11,12,13
+  printName() {
+    let name = "";
+    let valueName = "";
+    switch (this.value) {
+      case 1:
+        valueName = "ace";
+        break;
+      case 2:
+        valueName = "2";
+        break;
+      case 3:
+        valueName = "3";
+        break;
+      case 4:
+        valueName = "4";
+        break;
+      case 5:
+        valueName = "5";
+        break;
+      case 6:
+        valueName = "6";
+        break;
+      case 7:
+        valueName = "7";
+        break;
+      case 8:
+        valueName = "8";
+        break;
+      case 9:
+        valueName = "9";
+        break;
+      case 10:
+        valueName = "10";
+        break;
+      case 11:
+        valueName = "jack";
+        break;
+      case 12:
+        valueName = "queen";
+        break;
+      case 13:
+        valueName = "king";
+        break;
+      default:
+        valueName = "unknown";
+        break;
     }
 
-    printName() {
-        let name = "";
-        valueName = "";
-        switch (this.value) {
-            case 1:
-                valueName = "ace";
-                break;
-            case 2:
-                valueName = "two";
-                break;
-            case 3:
-                valueName = "three";
-                break;
-            case 4:
-                valueName = "four";
-                break;
-            case 5:
-                valueName = "five";
-                break;
-            case 6:
-                valueName = "six";
-                break;
-            case 7:
-                valueName = "seven";
-                break;
-            case 8:
-                valueName = "eight";
-                break;
-            case 9:
-                valueName = "nine";
-                break;
-            case 10:
-                valueName = "ten";
-                break;
-            case 11:
-                valueName = "jack";
-                break;
-            case 12:
-                valueName = "queen";
-                break;
-            case 13:
-                valueName = "king";
-                break;
-            default:
-                valueName = "UNKNOWN_VALUE_NAME";
-                break;
-        }
-
-        name = valueName + " of " + this.suit;
-        return name;
-    }
-
+    name = valueName + "_of_" + this.suit;
+    return name;
+  }
 }
