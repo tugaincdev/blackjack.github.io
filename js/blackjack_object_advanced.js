3305d8abd5b14e16ab44d10a66f762a15568ae1d
Mister-Lad <44650020+Mister-Lad@users.noreply.github.com>
2025-10-22 07:13:24 +0100

    Update blackjack_object_advanced.js
    compass now working perfectly too;
    
    lightning and quick inspect are visually represented and basic logic is done. mostly dont do anything though

--- js/blackjack_object_advanced.js
+++ js/blackjack_object_advanced.js
@@ -4,22 +4,9 @@ class Blackjack_Advanced extends Blackjack {
 
     this.powerUpList = {
       revealHidden: 0,
-      othersLater: 0,
+      compass: 0,
+      lightning: 0,
+      quick_inspect: 0,
     };
-
-    this.powerUpNameList = [
-      "./audio/LibraryOfRuinaBgmTheme02.mp3",
-      "./audio/LimbusCompanyOSTCasinoStoryTheme2.mp3",
-      "./audio/LimbusCompanyOSTCantoIIBattleThemeB2.mp3",
-      "./audio/LimbusCompanyCantoIIBattleThemeB1.mp3",
-      "./audio/LimbusCompanyOSTCantoIIBattleThemeA1.mp3",
-      "./audio/ClairObscurExpedition33MonocoOriginalSoundtrack.mp3",
-      "./audio/ClairObscurExpedition33OriginalSoundtrackBonusTrackFightfortheWinUnoPuncho.mp3",
-      "./audio/ClairObscurExpedition33OriginalSoundtrackBonusTrackGestralPrivateClub.mp3",
-      "./audio/LimbusCompanyIntervalloIV1BattleTheme.mp3",
-      "./audio/LimbusCompanyOSTTimeKillingTime.mp3",
-      "./audio/LimbusCompanyOSTObserveNotSee.mp3",
-      "./audio/LimbusCompanyOSTGoodDayDetective.mp3",
-    ];
   }
 }
